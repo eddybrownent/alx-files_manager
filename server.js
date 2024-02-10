c77619a5f0a927df9e3bb0a638bcd7e75070e82d
edwin karanja mburu <ekaranja.28@gmail.com>
2024-02-10 14:34:15 +0300

    creates and configure the main server for a Node.js application and defining how it handles incoming requests and responses

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import routeCont from './routes/index';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+routeCont(app);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default app;
