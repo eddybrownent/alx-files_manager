32df9e7251a112faa2de3f0593452ef6eb679c6a
edwin karanja mburu <ekaranja.28@gmail.com>
2024-02-13 18:12:05 +0300

    Update the endpoint POST /files endpoint to start a background processing for generating thumbnails for a file of type image

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -6,24 +6,21 @@ import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
 import userUtils from '../utils/users';
 import redisClient from '../utils/redis';
+import fileQueue from '../worker';
 
 class FilesController {
   static async postUpload(req, res) {
     try {
-      // gets user id using token
       const { userId } = await userUtils.getIdAndKey(req);
 
-      // if user not found returns Unauthorized
       if (!userId) {
         return res.status(401).send({ error: 'Unauthorized' });
       }
 
-      // gets required data from request body
       const {
         name, type, parentId = 0, isPublic = false, data,
       } = req.body;
 
-      // checking the fields if correct
       if (!name) {
         return res.status(400).send({ error: 'Missing name' });
       }
@@ -34,7 +31,6 @@ class FilesController {
         return res.status(400).send({ error: 'Missing data' });
       }
 
-      // checking is parentId is there
       if (parentId !== 0) {
         if (!ObjectId.isValid(parentId)) {
           return res.status(400).send({ error: 'Parent not found' });
@@ -48,7 +44,6 @@ class FilesController {
         }
       }
 
-      // prepare file document to insert into DB
       const fileDoc = {
         userId,
         name,
@@ -57,41 +52,26 @@ class FilesController {
         parentId,
       };
 
-      // file data for type=file|image
       if (type === 'file' || type === 'image') {
-        // decoding Base64 data
         const fileData = Buffer.from(data, 'base64');
-
-        // define a storing folder path
         const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
-
-        // creates a folder if not exists
         if (!fs.existsSync(folderPath)) {
           fs.mkdirSync(folderPath, { recursive: true });
         }
-
-        // makes a unique filename
         const fileName = `${uuidv4()}${path.extname(name)}`;
-
-        // diefine local path for the file
         const localPath = path.join(folderPath, fileName);
-
-        // writr the file to disk
         fs.writeFileSync(localPath, fileData);
-
-        // add a localPath to file document
         fileDoc.localPath = localPath;
+
+        // Add a job to the fileQueue for thumbnail generation
+        fileQueue.add({ userId: userId.toString(), fileId: fileDoc._id.toString() });
       }
 
-      // inserts file document into DB
       const result = await dbClient.db.collection('files').insertOne(fileDoc);
       const { insertedId } = result;
 
-      // removes _id field
       delete fileDoc._id;
       delete fileDoc.localPath;
-
-      // adds generated ID to file document
       fileDoc.id = insertedId;
 
       const responseObj = {
@@ -99,7 +79,6 @@ class FilesController {
         ...fileDoc,
       };
 
-      // return the new file with status code 201
       return res.status(201).send(responseObj);
     } catch (error) {
       console.error('Error uploading file:', error);
@@ -314,6 +293,7 @@ class FilesController {
 
   static async getFile(req, res) {
     const fileId = req.params.id;
+    const { size } = req.query; // Retrieve size parameter from query
 
     // get file using ID
     const file = await dbClient.db.collection('files').findOne({ _id: ObjectId(fileId) });
@@ -332,8 +312,13 @@ class FilesController {
       return res.status(400).send({ error: "A folder doesn't have content" });
     }
 
+    let filePath = file.localPath;
+    if (!Number.isNaN(Number(size)) && [500, 250, 100].includes(Number(size))) {
+      filePath += `_${size}`;
+    }
+
     // Check if the file is locally present
-    if (!fs.existsSync(file.localPath)) {
+    if (!fs.existsSync(filePath)) {
       return res.status(404).send({ error: 'Not found' });
     }
 
@@ -342,7 +327,7 @@ class FilesController {
 
     // Return the content of the file with the correct MIME-type
     res.setHeader('Content-Type', mimeType);
-    const fileStream = fs.createReadStream(file.localPath);
+    const fileStream = fs.createReadStream(filePath); // Use modified filePath
     fileStream.pipe(res);
 
     return res.status(404).send({ error: 'Not found' });
